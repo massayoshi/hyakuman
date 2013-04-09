266b505fc8db34a2770d3d1ccb29db7fd255a80d
Flávio M. Oota <f@oota.com.br>
2013-04-09 03:28:29 -0300

    belongs_to manufacturer removed from materials model

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,5 +1,5 @@
 class Material < ActiveRecord::Base
-  belongs_to :manufacturer#, :through => :productlines
+  #belongs_to :manufacturer, :through => :productline
   belongs_to :productline
   attr_accessible :image, :name, :productline_id, :manufacturer_id
   validates_presence_of :productline_id, :manufacturer_id
