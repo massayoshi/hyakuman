9d781611c0b160a10979e1c0ac0dd1373d0b64c6
Flávio M. Oota <f@oota.com.br>
2013-04-09 02:31:48 -0300

    activeadmin is back to the trash

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Hyakuman::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
-  resources :lines
-
-
   resources :productlines
   resources :manufacturers
   resources :materials
