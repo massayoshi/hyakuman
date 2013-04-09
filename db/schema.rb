b8078e7d5aa0af8a8b9f174bfbc9be44febb4ab9
Flávio M. Oota <f@oota.com.br>
2013-04-09 00:09:51 -0300

    activeadmin is back

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,50 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408084306) do
+ActiveRecord::Schema.define(:version => 20130409030647) do
+
+  create_table "active_admin_comments", :force => true do |t|
+    t.string   "resource_id",   :null => false
+    t.string   "resource_type", :null => false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.text     "body"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "namespace"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
+  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
+  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+
+  create_table "admin_users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
+  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "lines", :force => true do |t|
+    t.string   "name"
+    t.integer  "manufacturer_id"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "lines", ["manufacturer_id"], :name => "index_lines_on_manufacturer_id"
+
   create_table "manufacturers", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -21,10 +64,10 @@ ActiveRecord::Schema.define(:version => 20130408084306) do
   create_table "materials", :force => true do |t|
     t.string   "name"
     t.integer  "productline_id"
-    t.integer  "manufacturer_id"
     t.string   "image"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.integer  "manufacturer_id"
   end
 
   add_index "materials", ["productline_id"], :name => "index_materials_on_productline_id"
