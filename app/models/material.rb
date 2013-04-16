fb4853a6e1b29ae103463dfbefe0d44673914c2e
Flávio M. Oota <f@oota.com.br>
2013-04-16 00:31:18 -0300

    F it

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,6 @@
 class Material < ActiveRecord::Base
-  belongs_to :manufacturer, :through => :productline
   belongs_to :productline
+  belongs_to :manufacturer#, :through => :productline
   attr_accessible :image, :name, :productline_id, :manufacturer_id
   validates_presence_of :productline_id, :manufacturer_id
 end
